ab486644fc9d39c4c03245cfd37b8ebd2cc6ab97
xoferror <sadraa772@gmail.com>
2026-05-08 11:49:30 +0330

    changed jalali.go to use the latest api

--- backend/pkg/jalali.go
+++ backend/pkg/jalali.go
@@ -1,20 +1,26 @@
 package pkg
 
 import (
-    "time"
+	"fmt"
+	"time"
 
-    calendar "github.com/yaa110/go-persian-calendar"
+	ptime "github.com/yaa110/go-persian-calendar"
 )
 
 func GregorianToJalaliString(t time.Time) string {
-    p := calendar.NewPersian(t)
-    return p.String()
+	pt := ptime.New(t)
+	return pt.Format("yyyy/MM/dd")
 }
 
 func JalaliToGregorian(jalaliDate string) (time.Time, error) {
-    p, err := calendar.Parse(jalaliDate)
-    if err != nil {
-        return time.Time{}, err
-    }
-    return p.Time(), nil
+	// این پکیج Parse ندارد، باید manual parsing کنید
+	// یا از پکیج دیگری استفاده کنید
+	var year, month, day int
+	_, err := fmt.Sscanf(jalaliDate, "%d/%d/%d", &year, &month, &day)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	pt := ptime.Date(year, ptime.Month(month), day, 0, 0, 0, 0, ptime.Iran())
+	return pt.Time(), nil
 }
